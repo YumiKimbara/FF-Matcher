89a5e3379bbb267bb954a84e00d21b4be6f9fcf3
Yumi <a13158y@gmail.com>
2023-04-18 15:48:10 -0700

    delete comment out

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ const authRoutes = require("./routes/auth");
 app.use(
   cors({
     // origin: "http://ec2-35-183-29-247.ca-central-1.compute.amazonaws.com",
-    origin: "https://ff-matcher.onrender.com",
+    origin: ["http://localhost:3000", "https://ff-matcher.onrender.com"],
     credentials: true,
   })
 );
