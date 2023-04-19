f76b415581f4cb61862f58a17ec5fe925203d08b
Yumi <a13158y@gmail.com>
2023-04-18 17:15:17 -0700

    fix api

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -6,11 +6,7 @@ exports.getQuestions = async (req, res) => {
 
     return res
       .status(200)
-      .set(
-        "access-control-allow-origin",
-        "http://localhost:3000",
-        "https://ff-matcher.onrender.com"
-      )
+      .set("access-control-allow-origin", "http://localhost:3000")
       .json(questions);
   } catch (error) {
     res.status(404).json({ message: error.message });
